040996aa554f8435433d24348178e55f037f9da1
CARLOS VINICYUS SILVA NASCIMENTO <carlos.nascimento@grupojav.com.br>
2025-11-17 11:57:15 -0300

    :sparkles: feat: Criacao dos components da tela 'My Memories'

---
+++ app/src/main/java/com/example/memoraapp/ui/components/ExtendedFloatingActionButtonComponent.kt
@@ -0,0 +1,50 @@
+package com.example.memoraapp.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.memoraapp.ui.theme.MemoraAppTheme
+
+@Composable
+fun ExtendedFloatingActionButtonComponent(modifier: Modifier = Modifier) {
+    ExtendedFloatingActionButton(
+        onClick = {},
+        icon = { Icon(Icons.Filled.Add, "FAB Nova Memória", modifier.size(24.dp)) },
+        text = { Text(text = "Nova Memória", fontSize = 14.sp, fontWeight = FontWeight.SemiBold) },
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        shape = CircleShape,
+        modifier = modifier
+            .size(width = 210.dp, height = 56.dp)
+    )
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+private fun ExtendedFloatingActionButtonComponentView() {
+    MemoraAppTheme {
+        Surface(
+            color = MaterialTheme.colorScheme.background
+        ) {
+            ExtendedFloatingActionButtonComponent()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/memoraapp/ui/components/MemoryCardComponent.kt
@@ -0,0 +1,93 @@
+package com.example.memoraapp.ui.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.memoraapp.R
+import com.example.memoraapp.ui.theme.MemoraAppTheme
+
+@Composable
+fun MemoryCardComponent(
+    modifier: Modifier = Modifier,
+    title: String,
+    date: String
+) {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = modifier.size(width = 163.dp, height = 180.dp),
+        colors = CardDefaults.cardColors(MaterialTheme.colorScheme.surfaceVariant)
+    ) {
+        Column {
+            Image(
+                painter = painterResource(R.drawable.image_memorycard),
+                contentDescription = "Photo Memory Card",
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(90.dp)
+            )
+
+            Column(
+                modifier = modifier
+                    .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                    .size(width = 163.dp, height = 90.dp),
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold
+                    ),
+                )
+
+                Spacer(modifier.size(1.dp))
+
+                Text(
+                    text = date,
+                    style = MaterialTheme.typography.bodyMedium.copy(fontSize = 12.sp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+private fun MemoryCardComponentView() {
+    MemoraAppTheme {
+        Surface(
+            color = MaterialTheme.colorScheme.background
+        ) {
+            MemoryCardComponent(title = "Montanhas de Outono", date = "10 de Outubro, 2023")
+        }
+    }
+}

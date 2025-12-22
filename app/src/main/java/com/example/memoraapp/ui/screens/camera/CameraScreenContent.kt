94960bb27954c3f76fae79fbd0e81076dfa14bd3
CARLOS VINICYUS SILVA NASCIMENTO <carlos.nascimento@grupojav.com.br>
2025-12-22 11:22:39 -0300

    :sparkles: feat: Implementação do conteúdo do layout da câmera

---
+++ app/src/main/java/com/example/memoraapp/ui/screens/camera/CameraScreenContent.kt
@@ -0,0 +1,112 @@
+package com.example.memoraapp.ui.screens.camera
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cached
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallFloatingActionButton
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.memoraapp.ui.components.buttons.CaptureButton
+import com.example.memoraapp.ui.theme.MemoraAppTheme
+
+@Composable
+fun CameraScreenContent(
+    onCapture: () -> Unit,
+    onSwitchCamera: () -> Unit,
+    preview: @Composable () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = paddingValues.calculateTopPadding()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            HorizontalDivider(
+                thickness = 1.dp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 108.dp)
+            )
+
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(16.dp)
+                    .clip(RoundedCornerShape(16.dp))
+            ) {
+                preview()
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Spacer(Modifier.size(48.dp))
+
+                CaptureButton(onClick = onCapture)
+
+                SmallFloatingActionButton(
+                    onClick = onSwitchCamera,
+                    shape = CircleShape,
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                    contentColor = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier
+                        .size(56.dp)
+                ) {
+                    Icon(Icons.Default.Cached, null, modifier = Modifier.size(24.dp))
+                }
+            }
+        }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun CameraScreenView() {
+    MemoraAppTheme {
+        Surface(
+            color = MaterialTheme.colorScheme.background
+        ) {
+            CameraScreenContent(
+                onCapture = {},
+                onSwitchCamera = {},
+                preview = {}
+            )
+        }
+    }
+}

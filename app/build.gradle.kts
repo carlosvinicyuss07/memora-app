29eed54fb821175ab1734655d6924555a30df95a
CARLOS VINICYUS SILVA NASCIMENTO <carlos.nascimento@grupojav.com.br>
2025-12-24 11:13:32 -0300

    :heavy_plus_sign: build: Adicionando dependência para permissions da Câmera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,9 @@ dependencies {
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.view)
 
+    // Permissions
+    implementation(libs.accompanist.permissions)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)

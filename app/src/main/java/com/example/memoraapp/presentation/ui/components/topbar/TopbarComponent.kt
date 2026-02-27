15f338672ddd9ba724d4d77487a2659fe7538151
CARLOS VINICYUS SILVA NASCIMENTO <carlos.nascimento@grupojav.com.br>
2026-02-27 10:28:49 -0300

    :recycle: refactor: Alterações no componente da TopBar para adicionar opção de fazer logout/encerrar sessão

--- app/src/main/java/com/example/memoraapp/presentation/ui/components/topbar/TopbarComponent.kt
+++ app/src/main/java/com/example/memoraapp/presentation/ui/components/topbar/TopbarComponent.kt
@@ -3,6 +3,7 @@ package com.example.memoraapp.presentation.ui.components.topbar
 import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,11 +12,19 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -31,7 +40,9 @@ fun TopbarComponent(
     modifier: Modifier = Modifier,
     icon: ImageVector? = null,
     screenName: String,
-    onBackClick: () -> Unit = {}
+    onBackClick: () -> Unit = {},
+    onLogoutClick: () -> Unit = {},
+    iconMoreOptions: Boolean = false
 ) {
 
     val configuration = LocalConfiguration.current
@@ -41,35 +52,87 @@ fun TopbarComponent(
         modifier = modifier
             .height(108.dp)
             .fillMaxWidth()
-            .padding(bottom = 28.dp),
+            .padding(bottom = 21.dp),
         verticalAlignment = Alignment.Bottom,
-        horizontalArrangement = Arrangement.spacedBy(7.dp)
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
 
         val modifierIcon = if (isPortrait) {
-            modifier
+            Modifier
                 .width(48.dp)
                 .height(24.dp)
                 .padding(start = 24.dp)
         } else {
-            modifier
+            Modifier
                 .width(108.dp)
                 .height(24.dp)
                 .padding(start = 60.dp)
         }
 
-        Icon(
-            imageVector = icon ?: Icons.Filled.ArrowBackIosNew,
-            contentDescription = "IconToolbar",
-            tint = MaterialTheme.colorScheme.onBackground,
-            modifier = if (icon == null) modifierIcon.clickable { onBackClick() } else modifierIcon
-        )
+        val modifierIconMoreVert = if (isPortrait) {
+            Modifier
+                .width(48.dp)
+                .height(24.dp)
+                .padding(end = 24.dp)
+        } else {
+            Modifier
+                .width(108.dp)
+                .height(24.dp)
+                .padding(end = 60.dp)
+        }
 
-        Text(
-            text = screenName,
-            style = MaterialTheme.typography.bodyMedium.copy(fontSize = 18.sp, fontWeight = FontWeight.Bold),
-            color = MaterialTheme.colorScheme.onBackground
-        )
+        var expanded by remember { mutableStateOf(false) }
+
+        Row(
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.spacedBy(7.dp)
+        ) {
+            Icon(
+                imageVector = icon ?: Icons.Filled.ArrowBackIosNew,
+                contentDescription = "IconToolbar",
+                tint = MaterialTheme.colorScheme.onBackground,
+                modifier = if (icon == null) modifierIcon.clickable { onBackClick() } else modifierIcon
+            )
+
+            Text(
+                text = screenName,
+                style = MaterialTheme.typography.bodyMedium.copy(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
+
+        Box {
+            if (iconMoreOptions) {
+                IconButton(
+                    onClick = { expanded = true },
+                    modifier = modifierIconMoreVert
+                ) {
+
+                    Icon(
+                        imageVector = Icons.Filled.MoreVert,
+                        contentDescription = "IconToolbarMoreVert",
+                        tint = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+
+                    DropdownMenuItem(
+                        text = { Text("Encerrar sessão") },
+                        onClick = {
+                            expanded = false
+                            onLogoutClick()
+                        }
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -83,7 +146,7 @@ fun TopbarComponent(
 private fun TopbarComponentView() {
     MemoraAppTheme {
         Surface {
-            TopbarComponent(icon = Icons.Filled.Home, screenName = "Bem-vindo")
+            TopbarComponent(icon = Icons.Filled.Home, screenName = "Bem-vindo", iconMoreOptions = true)
         }
     }
 }
